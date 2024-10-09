cc69392aff0a3cad7c5866954e7aa02db3cd8abf
sarvarbekovich <khasanboyrumonov1998@yahoo.com>
2024-10-09 18:06:59 +0900

    feat: integration footer and basket source

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,15 +4,16 @@ import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { RippleBadge } from "./MaterialTheme/styled";
 import { Link, Route, Switch, useLocation } from "react-router-dom";
 
-import { HomePage } from "./screens/homePage";
-import { ProductsPage } from "./screens/productsPage";
-import { OrdersPage } from "./screens/ordersPage";
-import { UserPage } from "./screens/userPage";
-import { OtherNavbar } from "./components/headers/OtherNavbar";
-import { HomeNavbar } from "./components/headers/HomeNavbar";
-import { Footer } from "./components/footer";
+import HomePage from "./screens/homePage";
+import ProductsPage from "./screens/productsPage";
+import OrdersPage from "./screens/ordersPage";
+import UserPage from "./screens/userPage";
+import OtherNavbar from "./components/headers/OtherNavbar";
+import HomeNavbar from "./components/headers/HomeNavbar";
+import Footer from "./components/footer";
 import "../css/app.css";
 import "../css/navbar.css";
+import "../css/footer.css";
 import { HelpPage } from "./screens/helpPage";
 
 function App() {
